986646843994086f4bc415aabc331c6feeadc3cc
Alex <159346450+AlexandraSavenko@users.noreply.github.com>
2024-06-06 14:06:20 +0300

    task 5 done

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,12 @@
+const body = document.querySelector("body");
+const button = document.querySelector(".change-color");
+const color = document.querySelector(".color");
+button.addEventListener("click", handleColor);
+function handleColor() {
+  const resultColor = getRandomHexColor();
+  body.style.backgroundColor = resultColor;
+  color.textContent = resultColor;
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
