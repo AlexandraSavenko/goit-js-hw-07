213cb1c1c8015faa07ffeb24f50cb485d3b9af8d
Alex <159346450+AlexandraSavenko@users.noreply.github.com>
2024-06-06 11:19:43 +0300

    task 4 no reset

--- js/task-4.js
+++ js/task-4.js
@@ -2,8 +2,16 @@ const form = document.querySelector(".login-form");
 form.addEventListener("submit", handleGetInput);
 function handleGetInput(event) {
   event.preventDefault();
-  const formData = new FormData(event.currentTarget);
-  const data = {};
-  formData.forEach((value, key) => (data[key] = value));
-  console.log(data);
+  const { email, password } = event.currentTarget.elements;
+  if (email.value && password.value) {
+    const data = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(data);
+  } else {
+    alert("All form fields must be filled in");
+  }
+  //   email.value = "";
+  //   password.value = "";
 }
