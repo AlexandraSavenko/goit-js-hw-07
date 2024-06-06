312ebbc8b9dcd8daacbfc78ebf71cafa745fa42b
Alex <159346450+AlexandraSavenko@users.noreply.github.com>
2024-06-06 16:53:40 +0300

    task 2 done css

--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,42 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+// const controls = document.querySelector("#controls");
+// const buttonCreate = document.querySelector("#controls button[data-create]");
+// const buttonDestroy = document.querySelector("#controls button[data-destroy]");
+// const input = document.querySelector("input");
+
+// let inputValue;
+// input.addEventListener("input", handleInput);
+// function handleInput(event) {
+//   inputValue = event.currentTarget.value.trim();
+// }
+// console.log(inputValue);
+// // function createBoxes(amount) {
+// //   if ()
+// // }
+// // createBoxes();
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+const controls = document.querySelector("#controls");
+const buttonCreate = document.querySelector("#controls button[data-create]");
+const buttonDestroy = document.querySelector("#controls button[data-destroy]");
+const input = document.querySelector("input");
+
+let inputResult = 0; // Змінна має бути доступна глобально
+
+input.addEventListener("input", (event) => {
+  inputResult = event.currentTarget.value;
+  console.log(`Current input value: ${inputResult}`);
+});
+
+buttonCreate.addEventListener("click", () => {
+  createBoxes(inputResult);
+});
+
+function createBoxes(amount) {
+  console.log(`this is it ${amount}`);
 }
+
+// Тепер createBoxes викликається після натискання кнопки
