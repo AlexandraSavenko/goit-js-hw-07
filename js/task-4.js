ae8cb9fb13ff76cf79ebfa1910590fd9882a27f9
Alex <159346450+AlexandraSavenko@users.noreply.github.com>
2024-06-06 10:42:06 +0300

    task-4 not elements

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,9 @@
-
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", handleGetInput);
+function handleGetInput(event) {
+  event.preventDefault();
+  const formData = new FormData(event.currentTarget);
+  const data = {};
+  formData.forEach((value, key) => (data[key] = value));
+  console.log(data);
+}
