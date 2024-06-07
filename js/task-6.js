8345cb3378954770c2717dbbb9a1b6f59f14d541
Alex <159346450+AlexandraSavenko@users.noreply.github.com>
2024-06-07 12:26:35 +0300

    task 6 the boxes are added

--- js/task-6.js
+++ js/task-6.js
@@ -1,17 +1,41 @@
 const controls = document.querySelector("#controls");
 const input = document.querySelector("input");
-input.addEventListener("input", onInputChange);
-let Amount;
-function onInputChange(event) {
-  Amount = event.target.value;
+const createButton = document.querySelector("button[data-create]");
+const destroyButton = document.querySelector("button[data-destroy]");
+const boxes = document.querySelector("#boxes");
+
+input.addEventListener("input", handleInput);
+let inputResult = 0;
+function handleInput(event) {
+  inputResult = Number(event.currentTarget.value);
 }
-console.log(Amount);
-function createBoxes(amount) {
-  for (let i = amount; i <= 100; i += 1) {
-    console.log(`<div></div>`);
+
+//==============================================
+createButton.addEventListener("click", handlerCreate);
+function handlerCreate() {
+  if (inputResult >= 1 && inputResult <= 100) {
+    createBoxes(inputResult); //?but I can call this function with the right argument from here
   }
+  inputResult = 0;
+  input.value = "";
 }
-createBoxes(5);
+//============================================
+
+function createBoxes(amount) {
+  let boxHTML = "";
+  for (let i = 1; i <= amount; i += 1) {
+    boxHTML += `<div class="box"></div>`;
+  }
+  boxes.insertAdjacentHTML("beforeend", boxHTML);
+}
+// createBoxes(inputResult);//? for some reasone I can't use inputResult
+//?from here and I can only get it's value from the createButton function.
+//?Is that because input has no name ?
+//======================================================================
+destroyButton.addEventListener("click", handlerDestroy);
+function handlerDestroy() {
+  boxes.innerHTML = "";
+}
 //======================================================================
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
